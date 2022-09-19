767454940d6376fa81ebfc5a134e6135f6b6503a
killian31 <80256683+killian31@users.noreply.github.com>
2022-09-19 10:02:15 +0200

    small modif in classes

---
+++ transactions.py
@@ -0,0 +1,5 @@
+class Transaction:
+    def __init__(self, saller, buyer, amount):
+        self.seller = seller
+        self.buyer = buyer
+        self.amount = amount
--- users.py
+++ users.py
@@ -1,5 +1,6 @@
 from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
 class User:
     def __init__(self, name):
@@ -7,3 +8,8 @@ class User:
         self.private_key = rsa.generate_private_key(public_exponent=65537,key_size=2048)
         self.public_key = self.private_key.public_key()
     
+    def sign(self, message):
+        pass
+
+joe = User(name = "joe")
+joe.public_key.encrypt("test", PSS(mgf=MGF1(algorithm=HashAlgorithm().name), salt_lenth=PSS.DIGEST_LENGTH))
